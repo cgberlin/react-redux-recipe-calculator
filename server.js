b4cba7c65d9fa833d198068b04ea4d1310157650
cody <codyberlin@gmail.com>
2016-11-07 14:45:44 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+
+
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000, function onListen() {
+  const address = server.address();
+  console.log('Listening on: %j', address);
+  console.log(' -> that probably means: http://localhost:%d', address.port);
+});
